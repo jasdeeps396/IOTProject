a5e5adc84136a5a0a7e6efb1b35af9b09b3af22a
jasdeep singh <jasdeeps426@gmail.com>
2019-05-05 20:00:57 +0530

    admin login add

--- app.py
+++ app.py
@@ -29,12 +29,12 @@ def index():
         mq135values.append(dict_item['mq135'])
         mq7values.append(dict_item['mq7'])
         mq2values.append(dict_item['mq2']) 
-        # pm2_5values.append(dict_item['pm25'])
+        pm2_5values.append(dict_item['pm25'])
     mq135labels.reverse()
     mq135values.reverse()
     mq7values.reverse()
     mq2values.reverse()
-    # pm2_5values.reverse()
+    pm2_5values.reverse()
     if request.method=="POST":
         return redirect(url_for('/'))
     return render_template('index.html',dashactive="active",mqlabels = mq135labels ,mq135values = mq135values,mq7values = mq7values,mq2values=mq2values,pm2_5values=pm2_5values)
@@ -89,6 +89,19 @@ def pie():
         return redirect(url_for('/pie'))
     return render_template('pie.html',dashactive="active",mqlabels = mq135labels ,mq135values = mq135values,mq7values = mq7values,mq2values=mq2values,pm2_5values=pm2_5values)
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin1234':
+            error = 'Invalid Username or Password. Please try again.'
+        else:
+            return redirect(url_for('admin'))
+    return render_template('loginadmin.html',adminactive="active",error=error,)
+
+
+
 @app.route('/admin',methods=['POST','GET'])
 def admin():
     if request.method=="POST":
@@ -98,7 +111,7 @@ def admin():
     for person in subscribers:
         print(u'{} => {}'.format(person.id, person.to_dict()))
         subs[person.id]=person.to_dict()
-    return render_template('admin_dashboard.html',adminactive="active",subs=subs)
+    return render_template('admin_dashboard.html',subs=subs,adminactive="active")
 
 
 @app.route('/subscribe',methods=['POST','GET'])
@@ -127,9 +140,6 @@ def subscribe():
 
 
 
-# @app.route('/notifcation')
-# def noti():
-# 	return render_template('notifications.html',notifactive="active")
 
 
 @app.route('/Prediction',methods=['POST','GET'])
